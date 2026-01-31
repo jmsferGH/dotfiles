21264b7e243d3915ee5db1cc7c5fcb77cce2e8c3
jmsferGH <jmsfer@hotmail.com>
2026-01-31 18:50:25 +0000

    update to init.lua conf file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader= " "
+
 require("config.lazy")
 require("config.options")
 require("config.autocmds")
